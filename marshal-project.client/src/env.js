fe666592fd42c200aedd2aa36e0040df14afc840
justin <JustinCarpenter2020@Gmail.com>
2023-07-15 15:35:19 -0600

    env

--- marshal-project.client/src/env.js
+++ marshal-project.client/src/env.js
@@ -1,6 +1,6 @@
 export const dev = window.location.origin.includes('localhost')
 export const baseURL = dev ? 'http://localhost:3000' : ''
 export const useSockets = false
-export const domain = ''
-export const clientId = ''
-export const audience = ''
+export const domain = 'dev-kd4iv626qsy40as5.us.auth0.com'
+export const clientId = 'SYG6eiXGiL2mKS06FgB7tO0MWNeQNQ2n'
+export const audience = 'https://Shelfology.com'
